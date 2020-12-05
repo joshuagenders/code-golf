30cd868e63320bc84bca8c221cf6685e1d719c57
Joshua Genders <joshuagenders@hotmail.com>
2020-12-05 16:20:31 +0000

    initial commit

---
+++ huffman-tree/index.js
@@ -0,0 +1,91 @@
+import fs from 'fs'
+
+const priorityQueue = (items=[]) => ({
+    push(item){
+        for (const [x, i] in items){
+            if (item.count < x.count){
+                items.splice(i, 0, item)
+                return
+            }
+        }
+        items.push(item)
+    },
+    pop: () => items.pop(),
+    items
+})
+
+const getCharacterCount = (chars) => {
+    let counts = {}
+    for (let i = 0; i < 256; i++){
+        counts[String.fromCharCode(i)] = 0
+    }
+    for (const char of chars){
+        counts[char]++
+    }
+    const pq = priorityQueue()
+    for (const [char, count] of Object.entries(counts)) {
+        if (count > 0)
+            pq.push(treeNode(undefined, undefined, count, char), count)
+    }
+    return pq
+}
+
+const buildTree = (charPq) => {
+    while (charPq.items.length > 1) {
+        let r = charPq.pop()
+        let l = charPq.pop()
+        let newNode = treeNode(l, r, l.count + r.count, undefined)
+        charPq.push(newNode, newNode.count)
+    }
+    return charPq.items[0]
+}
+
+const treeNode = (left, right, count, value) => ({
+    left,
+    right,
+    count,
+    value,
+})
+
+const buildCharacterTranslation = (currentNode, currentPath=[]) => {
+    if (currentNode.value) {
+        return [{char: currentNode.value, charPath: currentPath}]
+    }
+    const paths = []
+    if (currentNode.left){
+        paths.push(...buildCharacterTranslation(currentNode.left, [...currentPath, 0]))
+    }
+    if (currentNode.right){
+        paths.push(...buildCharacterTranslation(currentNode.right, [...currentPath, 1]))
+    }
+    return paths
+}
+
+const characterTranslationTable = (rootNode) => {
+    const translations = buildCharacterTranslation(rootNode)
+    const t = new Map()
+    for (const translation of translations){
+        t.set(translation.char, translation.charPath)
+    }
+    return t
+}
+
+export const encode = (inputPath, outputPath) => {
+    const data = fs.readFileSync(inputPath, { encoding: 'ascii' }).toString()
+    const charCountsPriorityQueue = getCharacterCount(data)
+    const root = buildTree(charCountsPriorityQueue)
+    const table = characterTranslationTable(root)
+    const throughTranslationTable = datum => table.get(datum)
+    const output = [...data].map(throughTranslationTable)
+    console.log({ output })
+    //todo store translation table
+    // fs.writeFileSync(output.join())
+}
+
+const decode = () => {
+    //decode translation table
+    //run data back through translation table
+    //output text to file
+}
+
+encode('test.txt', '')
