53d030df9172791a84a5f9b2428ca540a48e829a
Joshua Genders <joshuagenders@hotmail.com>
2020-12-06 19:33:46 +0000

    completely remove lz leftovers

--- huffman-tree-python/main.py
+++ huffman-tree-python/main.py
@@ -1,68 +1,13 @@
 
 import heapq
-import json
 import itertools
+import json
 from functools import reduce
+from json import JSONEncoder
 
-def lz(data):
-    d = {i:chr(i) for i in range(255)}
-    output = []
-    int_data = [int(b) for b in bytes(data, 'utf-8')]
-    counter = 256
-    current_window = 1
-    
-    for i in range(0, len(int_data), current_window):
-        current = chr(int_data[i:i + current_window][0])
-        x = list(map(chr, int_data[i:i + current_window + 1]))
-        nxt = ''.join([current] + x)
-        if (nxt in d.values()):
-            for k, v in d.items():
-                if v == nxt:
-                    output.append((int(k) & 0x0000FF00) >> 8)
-                    output.append(int(k) & 0x000000FF)
-                    break
-        else:
-            d[counter] = nxt
-            counter += 1
-            output.append(ord(current))
-            output.append(0)
-
-    byte_output = []
-    half_remaining = False
-    for i in range(len(output)):
-        is_dict_bits = i % 2 == 1
-        if not half_remaining:
-            if is_dict_bits:
-                byte_output.append(output[i] << 4 & 0x00000010)
-                half_remaining = True
-            else:
-                byte_output.append(output[i])
-        else:
-            if is_dict_bits:
-                byte_output[-1:] = [byte_output[-1:][0] ^ output[i]]
-                half_remaining = False
-            else:
-                byte_output[-1:] = [byte_output[-1:][0] ^ (output[i] >> 4)]
-                byte_output.append((output[i] << 4) & 0x00000010)
-                half_remaining = True
-    # print(byte_output)
-    return bytes(byte_output)
-
-def unlz(data):
-    # construct d (table) - lookups for 0-255
-    # for int in byte pairs
-    #   if val in table
-    #     add to table
-    #   perform lookup
-    #   write output
-
-    pass
-
-def decode_data():
-    # parse tree from file
-    # parse data into byte array
-    # unzip byte array
-    pass
+class DefaultEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__  
 
 class Node:
     def __init__(self, left, right, count, value):
@@ -72,8 +17,6 @@ class Node:
         self.value = value
     def __lt__(self, other):
         return self.count < other.count
-    # def __str__(self):
-    #     return (f'Node: {self.value} count {self.count} \n  left: {self.left is not None}\n right: {self.right is not None}\n')
 
 def chunk_to_byte(b):
     return 1 * b[0] + 2 * b[1] + 4 * b[2] + 8 * b[3] + 16 * b[4] + 32 * b[5] + 64 * b[6] + 128 * b[7]
@@ -92,14 +35,10 @@ def encode(input_file, output_file):
         data = f.read()
     print(f'File: {input_file}')
     print(f'File size: {len(data)}')
-    zipped = lz(data)
-    # print(zipped)
-
-    print(f'Zipped size: {len(zipped)}')
-    # print(zipped)
+    
     q = []
     for i in range(256):
-        count = bytes(zipped).count(i)
+        count = bytes(data, 'utf-8').count(i)
         if (count):
             value = i
             n = Node(None, None, count, value)
@@ -112,15 +51,15 @@ def encode(input_file, output_file):
     root = q[0][1]
     translation_list = build_character_translation(root, [])
     translation = {k:v for list_item in translation_list for (k,v) in list_item.items()}
-    encoded = list(itertools.chain(*[translation.get(n) for n in zipped]))
-    # print(*encoded)
-   
+    encoded = list(itertools.chain(*[translation.get(n) for n in bytes(data, 'utf-8')]))
+    
     encoded_bytes = bytes(chunk_to_byte(b) for b in byte_chunks(encoded))
-    # serialisedTree = json.dumps(root)
-    # print(serialisedTree)
-    print(f'Encoded size: {len(encoded_bytes)}')
+    serialisedTree = DefaultEncoder().encode(root)
+    output = bytes(serialisedTree, 'utf-8') + encoded_bytes
+    print(f'Encoded size: {len(output)}')
+    print(f'Ratio: {len(output) / len(data)}')
     with open(output_file, 'wb') as f:
-        f.write(encoded_bytes)
+        f.write(output)
 
 def build_character_translation(start_node, current_path=[]):
     if start_node.value is not None:
@@ -136,8 +75,45 @@ def build_character_translation(start_node, current_path=[]):
         paths.extend(build_character_translation(start_node.right, new_path))
     return paths
 
+def decode_data(input_file):
+    with open(input_file, 'rb') as f:
+        data = f.read()
+    opening = ord('{')
+    closing = ord('}')
+    openingCount = 0
+    start = data.index(opening)
+    for i in range(start, len(data)):
+        val = data[i]
+        if (opening == val):
+            openingCount += 1
+        if (closing == val):
+            openingCount -= 1
+        if (openingCount == 0):
+            data_start = i + 1
+            break
+    tree_data = data[:data_start]
+    encoded_data = data[data_start:]
+    tree = json.loads(tree_data)
+    read_bit = lambda i, bit: (encoded_data[i] >> bit) % 2
+    current_node = tree
+    output = ''
+    for i in range(len(encoded_data)):
+        bit = read_bit(i // 8, i % 8)
+        if bit:
+            if current_node['right']:
+                current_node = current_node['right']
+        else:
+            if current_node['left']:
+                current_node = current_node['left']
+        if current_node['value']:
+            output += chr(current_node['value'])
+            current_node = tree
+    return output
+
 def decode(input_file, output_file):
-    pass
+    data = decode_data(input_file)
+    with open(output_file, 'w', encoding="utf-8") as f:
+        f.write(data)
 
 if __name__ == "__main__":
     encode('./testfiles/tale.txt', 'output.bin')
