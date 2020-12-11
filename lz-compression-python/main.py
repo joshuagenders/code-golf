dea3d4c27db7ecb4124eac5efbab398de7825ca5
Joshua Genders <joshuagenders@hotmail.com>
2020-12-11 22:02:38 +0000

    cleanup

--- lz-compression-python/main.py
+++ lz-compression-python/main.py
@@ -5,27 +5,30 @@ def compress(d: bytes):
     bit_count = 10
     compressed_int = lambda x: BitArray(x.to_bytes(2, 'big'))[16 - bit_count:]
     int_to_8bits = lambda x: BitArray(x.to_bytes(1, 'big'))
+    bit_index = lambda byte_index: byte_index * 8
+
     dictionary = {int_to_8bits(i).bin:compressed_int(i) for i in range(255)}
+    counter = 256
     data = BitArray(d)
     output = BitArray()
-    byte_count = len(data) // 8
+    data_bit_count = len(data)
+    data_byte_count = data_bit_count // 8
     byte_index = 0
-    counter = 256
-    
-    while byte_index < byte_count:
+
+    while byte_index < data_byte_count:
         current_window = 8
-        current_bytes = data[byte_index*8:byte_index*8 + current_window]
-        next_bytes = data[byte_index*8:byte_index*8 + current_window]
+        index = bit_index(byte_index)
+        current_bytes = data[index:index + current_window]
+        next_bytes = data[index:index + current_window]
         while next_bytes.bin in dictionary:
-            current_bytes = next_bytes
-            if byte_index + current_window // 8 > byte_count:
+            if index + current_window > data_bit_count:
                 break
-            else:
-                current_window += 8
-                next_bytes = data[byte_index*8:byte_index*8 + current_window]
+            current_bytes = next_bytes
+            current_window += 8
+            next_bytes = data[index:index + current_window]
 
         # if there is a next byte and there is room in the dictionary then add to dictionary
-        if byte_index + current_window // 8 < byte_count and counter < 2 ** bit_count:
+        if index + current_window < data_bit_count and counter < 2 ** bit_count:
             byte_val = BitArray(counter.to_bytes(2, 'big'))[16 - bit_count:]
             dictionary[next_bytes.bin] = byte_val
             counter += 1
