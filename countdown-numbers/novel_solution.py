3a0ef610b33cfd8f913ed31760c6f6ac4aea34bf
Joshua Genders <joshuagenders@hotmail.com>
2021-01-02 18:50:04 +0000

    wip

--- countdown-numbers/novel_solution.py
+++ countdown-numbers/novel_solution.py
@@ -38,6 +38,8 @@ operators = ('+', '-', '/', '*')
 
 target_min = 100
 target_max = 999
+num_choices = 3 # 6
+
 
 operator_table = {
     '+': lambda x, y: y + x,
@@ -50,101 +52,110 @@ nodes = {}
 
 def get_or_create_node(number):
     if number not in nodes:
+        print (f'creating node for {number}')
         nodes[number] = Node(number)
     return nodes[number]
 
 ### a sequence of operations
 ## the operator and number at each step
-class Equation:
-    def __init__(self, current_value, operations = []):
-        # [[number, operation]]
-        self.operations = operations
-        self.initial_value = current_value
-        self.current_value = current_value
+# class Equation:
+#     def __init__(self, current_value, operations = []):
+#         # [[number, operation]]
+#         self.operations = operations
+#         self.initial_value = current_value
+#         self.current_value = current_value
 
-    def append(self, operation, number):
-        new_value = operator_table[operation](number, self.current_value)
-        if not float(new_value).is_integer() or new_value < 0:
-            return False
-        # ensure doesn't include more than one of large set, or two of small set
-        count = sum([o for o in self.operations if o[0] == number])
-        if number in large_set and count < 2:
-            return Equation(new_value, [self.operations, [number, operation]])
-        elif number in small and count == 0:
-            return Equation(new_value, [self.operations, [number, operation]])
-        return False
+#     def append(self, operation, number):
+#         new_value = operator_table[operation](number, self.current_value)
+#         if not float(new_value).is_integer() or new_value < 0:
+#             return False
+#         # ensure doesn't include more than one of large set, or two of small set
+#         count = sum([o for o in self.operations if o[0] == number])
+#         if number in large_set and count < 2:
+#             return Equation(new_value, [self.operations, [number, operation]])
+#         elif number in small and count == 0:
+#             return Equation(new_value, [self.operations, [number, operation]])
+#         return False
 
 class Node:
     def __init__(self, number):
         self.number = number
         self.equations = []
 
+    def __str__(self):
+        return f'{self.number} : {self.equations}'
     def operate_with_all(self):
-        for x in unique_set:
-            for x in operators:
-            pass
-        for equation in self.equations:
-            pass
+        if self.number == 0:
+            return
+        # combinations = []
+        # for num in unique_set:
+        #     for op in operators:
+        #         combinations.append([op, num])
+        
+        # for equation in self.equations:
+        #     pass
 
 
         # perform operations with number
         # store valid equations 
-        # results = []
-        # for x in unique_set:
-        #     # result, operator, 
-        #     results.append((x + self.number, '+'))
-        #     results.append((x - self.number, f'-{x}'))
-        #     if x != 1:
-        #         results.append((x / self.number, f'/{x}'))
-        #         results.append((x * self.number, f'*{x}'))
-        # valid_results = [result for result in results if float(result[0]).is_integer() and result[0] >= 0]
+        results = []
+        for x in unique_set:
+            # result, operator, 
+            results.append((x + self.number, ('+', x)))
+            results.append((x - self.number, ('-', x)))
+            if x != 1 and x != 0:
+                results.append((x / self.number, ('/', x)))
+                results.append((x * self.number, ('*', x)))
+        valid_results = [result for result in results if float(result[0]).is_integer() and result[0] >= 0]
+
         # print(f'valid results {valid_results}')
         # # all of this nodes equations, combined with the equations to the new nodes
-        # for result in valid_results:
-        #     node = get_or_create_node(result[0])
-        #     for equation in self.equations:
-        #         node.equations.append(equation + result[1])
-        #         jj = equation + result[1]
-        #         print(f'equation: {node.number},  {jj}')
-        #         # todo, how to prevent 3*3*3*3*3*3 etc.
+        for result in valid_results:
+            node = get_or_create_node(result[0])
+            for equation in self.equations:
+                # print(f'equation: {equation}')
+                if len(equation) > num_choices - 1:
+                    continue
+                new_number = result[1][1]
+                count = sum(1 for x in equation if x[1] == new_number)
+                new_equation = equation[::]
+                new_equation.append(result[1])
 
-                # existing_numbers_in_equation = [i[0] for i in equation]
-                
-                # number_being_operated_with = result[2]
-                # if number_being_operated_with in large_set:
-                #     # ensure doesn't include more than one of large set
-                #     if not number_being_operated_with in existing_numbers_in_equation:
-                #         node.equations.append((equation + [(result[1], result[2])]))
-                # else:
-                #     # ensure doesn't include more than two of small set
-                #     if sum([i for i in existing_numbers_in_equation if i == number_being_operated_with]) < 2:
-               
+                if new_number in large_set and count < 2:
+                    node.equations.append(new_equation)
+                    # print(f'{self.number},{result[0]},{new_equation}')
+                if new_number in small_set and count == 0:
+                    new_equation.append(result[1])
+                    node.equations.append(new_equation)
+                    # print(f'{self.number},{result[0]},{new_equation}')
 
-
+            # break
 
 if __name__ == '__main__':
     initial_nodes = [Node(node) for node in unique_set]
-
-    # initial nodes should contain a single path, themselves as a number
+#
+    # initial nodes should contain a single equation, + n
     for node in initial_nodes:
-        node.equations = [f'{node.number}']
+        node.equations = [[('+', node.number)]]
         nodes[node.number] = node
 
-    rounds = 3
+    rounds = num_choices
     for r in range(rounds - 1):
         # take a copy of the list as it will be modified
         n = dict(nodes)
         for k,node in n.items():
             node.operate_with_all()
-        break    
+        # break    
 
     # final round nodes
     result_nodes = [ node for k,node in nodes.items() if node.number >= target_min and node.number <= target_max ]
     for result in result_nodes:
         result.operate_with_all()
 
-    results = [ result for result in x.equations for x in result_nodes ]
-    print(len(results))
+    results = [ result for sublist in result_nodes for sublist in result.equations ]
+    # [item for sublist in regular_list for item in sublist]
+    print(*results)
+    print('done')
 
 
 
